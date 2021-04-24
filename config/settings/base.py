c1e8a83ea225e8344a4660c2dee549e5a7ee967a
Julian Archila <julialejo2018@gmail.com>
2021-04-24 16:01:54 -0500

    Django rest framework. List circles view

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,6 +48,7 @@ DJANGO_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
+    "rest_framework"
 ]
 LOCAL_APPS = [
     "cride.users.apps.UsersAppConfig",
--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,16 +1,26 @@
 """ Circle views. """
 
-# Django 
-from django.http import HttpResponse
+# django rest framework. 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Models
 from cride.circles.models import Circle
+from django.http import JsonResponse
 
+@api_view(["GET"])
 def list_circles(request):
-    circles = Circle.objects.all()
-    public = circles.filter(is_public=True)
+    # import ipdb; ipdb.set_trace()
+    circles = Circle.objects.filter(is_public=True)
     data = []
-    for circle in public:
-        print(circle)
+    for circle in circles:
+        data.append({
+            "name": circle.name,
+            "slug_name": circle.slug_name,
+            "rides_taken": circle.rides_taken,
+            "rides_offered": circle.rides_offered,
+            "members_limit": circle.members_limit
 
-    return HttpResponse("Hello")
+        })
+
+    return Response(data)
