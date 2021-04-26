de94fd90de4a8bd4692a4c14df448824d7fd86fd
Julian Archila <julialejo2018@gmail.com>
2021-04-26 10:34:55 -0500

    Limit login for users with unverified accounts.

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -66,7 +66,7 @@ class UserSignUpSerializer(serializers.Serializer):
 
     def create(self,data):
         data.pop("password_confirmation")
-        user = User.objects.create_user(**data)
+        user = User.objects.create_user(**data, is_verified=False)
         profile = Profile.objects.create(user=user)
         return user
 
@@ -83,6 +83,9 @@ class UserLoginSerializer(serializers.Serializer):
         user = authenticate(username=data["email"], password=data["password"])
         if not user:
             raise serializers.ValidationError("Invalid credentials.")
+        if not user.is_verified:
+            raise serializers.ValidationError("Account is not active yet :(")
+
         self.context["user"] = user
         return data
 
